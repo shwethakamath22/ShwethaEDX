123d8de458748e5d678662d89cf9eb7a25644b45
shwethakamath22 <85861950+shwethakamath22@users.noreply.github.com>
2021-06-16 22:42:27 +0530

    Update EDXscript.js

--- EDXscript.js
+++ EDXscript.js
@@ -1,1 +1,19 @@
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
 
+function compute(){
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);
+    var interest = (principal*years*rate)/100;
+    var amount=parseFloat(interest)+parseFloat(principal);
+
+    if(principal <= 0 || principal == ""){
+        alert("Enter positive value for principal amount");
+        document.getElementById("principal").focus();
+    }
+    document.getElementById("result").innerHTML="\<br/>If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+}
